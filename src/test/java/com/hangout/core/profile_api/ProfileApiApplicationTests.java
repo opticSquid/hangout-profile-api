b59cecc3ebc7ffb92de4928fd2f9ba4c90331ee7
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2025-02-22 06:11:31 +0530

    Initial Commit

---
+++ src/test/java/com/hangout/core/profile_api/ProfileApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hangout.core.profile_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
